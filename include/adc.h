a3c954b601e22a626b814f5582c8cba88a09bbe8
Mike <ferlandmikael@gmail.com>
2015-11-17 12:52:46 -0500

    Header changed

--- include/adc.h
+++ include/adc.h
@@ -1,8 +1,11 @@
-/**
-	@file adc.h
-	@brief Header pour le fichier adc.c
-	@author Mikael Ferland
-	@date 07 Octobre 2015
+/*
+    Title:    main.c
+    Authors:  Mikael Ferland, Joël Brisson
+    Date:     07/10/2015
+    Purpose:  Contain function for adc
+    Software: AVR-GCC to compile
+    Hardware: ATMega32 on STK500 board
+    Note:     
 */
 #ifndef __ADC_H_
 #define __ADC_H_
