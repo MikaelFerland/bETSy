f3cee2df1c17f5b09ae826eeaa4317479561b9f9
nitrolectro <joel.brisson@hotmail.com>
2015-11-17 13:11:49 -0500

    Update timer.h

--- include/timer.h
+++ include/timer.h
@@ -1,3 +1,13 @@
+/*
+    Title:    timer.h
+    Authors:  Mikael Ferland, Joël Brisson
+    Date:     07/10/2015
+    Purpose:  Header for timer.c
+    Software: AVR-GCC to compile
+    Hardware: ATMega32 on STK500 board
+    Note:     
+*/
+
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
@@ -8,8 +18,6 @@
 
 /* Global functions */
 extern void timer_Init           (void);
-
-/* Macros */
 
 
 #endif
